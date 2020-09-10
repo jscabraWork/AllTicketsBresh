0a3f6dd8d0eb0adcb5aa1aa38fb563dd77859450
juan cabra <js.cabra@uniandes.edu.co>
2020-09-10 18:34:12 -0500

    Errores

--- src/app/ciudades/ciudades.module.ts
+++ src/app/ciudades/ciudades.module.ts
@@ -1,5 +1,7 @@
+import { ResumirPipe6 } from './../pipes/resumir.pipe copy 4';
 import { ResumirPipe4 } from './../pipes/resumir.pipe copy 3';
 import { ResumirPipe3 } from './../pipes/resumir.pipe copy 2';
+
 
 import { HttpClientModule } from '@angular/common/http';
 import { CiudadesEventosComponent } from './ciudades-eventos/ciudades-eventos.component';
@@ -35,6 +37,7 @@ const routes: Routes=[
 
     declarations:[CiudadesEventosComponent,
     ResumirPipe3,
+    ResumirPipe6,
   ResumirPipe4 ],
     imports:[
         CommonModule,
