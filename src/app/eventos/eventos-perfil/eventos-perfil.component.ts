0966c28b79c2bd0487d797a6856f85f18ac8d086
Juan Sebastian Cabra Leon <js.cabra@uniandes.edu.co>
2021-02-17 15:26:21 -0500

    final

--- src/app/eventos/eventos-perfil/eventos-perfil.component.ts
+++ src/app/eventos/eventos-perfil/eventos-perfil.component.ts
@@ -157,39 +157,5 @@ darCantidadDePalcos(localidad:Localidad){
   return contador;
 }
 
-window: any = window;
-handler = this.window.ePayco.checkout.configure({ key: '491d6a0b6e992cf924edd8d3d088aff1', test: true });
-epayco(){
-
-
-    var data={ 
-      //Parametros compra (obligatorio) 
-      invoice: "1234",
-      currency: "cop",
-      name: "Plan de facturacion electronica",
-      description: "Plan de facturacion electronica",
-      tax_base: '0',
-      tax: '0',
-      amount: 40000,
-      country: "co",
-      lang: "es",
-    external: "false",
-    //Onpage="false" - Standard="true" 
-      //Atributos opcionales
-      method:'GET',
-      extra1: '',
-      extra2: 'ePayco',
-      extra3: '',
-      response: "http://localhost:4200/eventos/respuesta",
-      confirmation: "http://localhost:4200/eventos/confirmacion",
-  } 
-  
-  this.handler.onCloseModal = this.onCloseEpaycoModal
-  this.handler.open(data)
-}
-
-onCloseEpaycoModal(){
-  alert('Close ePayco Modal!!!!!!!')
-}
 
 }
