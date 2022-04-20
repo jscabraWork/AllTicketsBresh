755379d191ed7d9a725f8224b404ce611c80fe59
LearsacaZ <learsacaz@hotmail.com>
2022-04-20 15:56:44 -0500

    mapa 7

--- src/app/eventos/eventos-perfil/pago-pay-u/comprar-palcos/comprar-palcos.component.ts
+++ src/app/eventos/eventos-perfil/pago-pay-u/comprar-palcos/comprar-palcos.component.ts
@@ -978,8 +978,11 @@ export class ComprarPalcosComponent implements OnInit {
                   this.localidadCargadaPreferecial = this.etapas[i].localidades[j];
                 }
 
-                else if (this.etapas[i].localidades[j].nombre == 'GENERAL') {
+                else if (this.etapas[i].localidades[j].nombre == 'General') {
                   this.localidadCargadaGeneral = this.etapas[i].localidades[j];
+                }
+                else if (this.etapas[i].localidades[j].nombre == 'Meet and Greet') {
+                  this.localidadCargadaBoletasVIP = this.etapas[i].localidades[j];
                 }
               
               }
@@ -1006,11 +1009,11 @@ export class ComprarPalcosComponent implements OnInit {
                   this.localidadCargadaPreferecial = this.etapas[i].localidades[j];
                 }
 
-                else if (this.etapas[i].localidades[j].nombre == 'VIP') {
+                else if (this.etapas[i].localidades[j].nombre == 'VIP 1ra Etapa') {
                   this.localidadCargadaBoletasVIP = this.etapas[i].localidades[j];
                 }
                 
-                else if (this.etapas[i].localidades[j].nombre == 'GENERAL') {
+                else if (this.etapas[i].localidades[j].nombre == 'General 1ra Etapa') {
                   this.localidadCargadaGeneral = this.etapas[i].localidades[j];
                 }
               
