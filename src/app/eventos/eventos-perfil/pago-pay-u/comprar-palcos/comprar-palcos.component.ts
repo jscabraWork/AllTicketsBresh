ef6eecc603a578e7353142321c6142704be2066a
LearsacaZ <learsacaz@hotmail.com>
2022-04-21 10:56:55 -0500

    mapa9

--- src/app/eventos/eventos-perfil/pago-pay-u/comprar-palcos/comprar-palcos.component.ts
+++ src/app/eventos/eventos-perfil/pago-pay-u/comprar-palcos/comprar-palcos.component.ts
@@ -1024,6 +1024,34 @@ export class ComprarPalcosComponent implements OnInit {
           });
         }
 
+        else if (this.evento.mapa == 'mapa9') {
+          this.etapaServicio
+          .getAllEtapasVisiblesDeEvento(this.evento.id, true)
+          .subscribe((response) => {
+
+            this.etapas = response;
+
+            for (let i = 0; i < this.etapas.length; i += 1) {
+              for (let j = 0; j < this.etapas[i].localidades.length; j += 1) {
+
+
+                if (this.etapas[i].localidades[j].nombre == 'PALCOS') {
+                  this.localidadCargadaPreferecial = this.etapas[i].localidades[j];
+                }
+
+                else if (this.etapas[i].localidades[j].nombre == 'GENERAL') {
+                  this.localidadCargadaGeneral = this.etapas[i].localidades[j];
+                }
+              
+              }
+            }
+
+
+            this.cargarLocalidadEnMapa9();
+            this.cargadoTodo = true;
+          });
+        }
+
 
         else if (this.evento.mapa == 'mapa22') {
           this.etapaServicio
@@ -1994,6 +2022,44 @@ export class ComprarPalcosComponent implements OnInit {
         }
       } else if (this.localidadCargadaPreferecial.palcos[i+3].proceso) {
         this.lista2[i]= {
+          valor:'p',
+          localidad: 'oro',
+          id:'p'
+        }
+      }
+
+    }
+
+  }
+
+  cargarLocalidadEnMapa9(){
+  
+    for(let i=0;i<3;i++){
+      if (
+        !this.localidadCargadaPreferecial.palcos[i].vendido &&
+        !this.localidadCargadaPreferecial.palcos[i].reservado &&
+        this.localidadCargadaPreferecial.palcos[i].disponible &&
+        !this.localidadCargadaPreferecial.palcos[i].proceso
+      ){
+        this.lista1[i] = {
+          valor:this.localidadCargadaPreferecial.palcos[i].numeroDentroDeEvento,
+          localidad: 'oro',
+          id:this.localidadCargadaPreferecial.palcos[i].id
+        }
+      }
+      
+      else if (
+        this.localidadCargadaPreferecial.palcos[i].vendido ||
+        this.localidadCargadaPreferecial.palcos[i].reservado ||
+        !this.localidadCargadaPreferecial.palcos[i].disponible
+      ) {
+        this.lista1[i] =  {
+          valor:'v',
+          localidad: 'oro',
+          id:'v'
+        }
+      } else if (this.localidadCargadaPreferecial.palcos[i].proceso) {
+        this.lista1[i]= {
           valor:'p',
           localidad: 'oro',
           id:'p'
