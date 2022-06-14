69b47de3b4f2615963ecd4a89de35c00444eba5f
learsacaz <learsacaz@hotmail.com>
2022-06-13 19:39:22 -0500

    grupoaval

--- src/app/eventos/eventos-perfil/pago-pay-u/carrito-de-compras/grupo-aval/grupo-aval.component.ts
+++ src/app/eventos/eventos-perfil/pago-pay-u/carrito-de-compras/grupo-aval/grupo-aval.component.ts
@@ -40,29 +40,41 @@ export class GrupoAvalComponent implements OnInit {
 
   manejar(){
 
-    
     if(!this.cargando){
+      let arreglo =  this.tarjeta.cardNumber.split(" ")
+      this.tarjeta.cardNumber = ""
+      for(let i=0;i<0;i++){
+        if(arreglo[i]!=" "){
+          this.tarjeta.cardNumber += arreglo[i]
+        }
+      }
       this.cargando = true
     this.servicio.crearToken(this.token, this.tarjeta).subscribe(response=>{
       
       this.tokenT = response.data.id
-      this.mask = response.data.card.mask
-      this.bin = this.mask.split("*")[0]
-
-      this.binservice.getBin(this.mask).subscribe(response=>{
-
-        if(response!=null){
-          this.dialogRef.close(true)
-        }
-        else{
+      if(response.data.card){
+        this.mask = response.data.card.mask
+        this.bin = this.mask.split("*")[0]
+  
+        this.binservice.getBin(this.bin).subscribe(response=>{
+  
+          if(response!=null){
+            this.dialogRef.close(true)
+          }
+          else{
+            this.dialogRef.close(false)
+          }
+        },
+        error=>{
+          error
           this.dialogRef.close(false)
         }
-      },
-      error=>{
-        error
-        this.dialogRef.close(false)
+        )
+      }else{
+        alert(response.data.error.errors)
+        this.cargando=false
       }
-      )
+      
     },
     error=>{
       error
